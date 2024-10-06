4894e129c5ff4e543b9c781ff92a9514f2d4dca3
Wolffyx <wolffdev@gmail.com>
2024-10-07 00:04:48 +0300

    Round 2 v2

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/Wolffyx.github.io",
+
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
 };
 
 // export default withSentryConfig(nextConfig, {
